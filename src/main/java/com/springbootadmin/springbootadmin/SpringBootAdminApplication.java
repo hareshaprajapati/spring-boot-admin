ee3795b870e7bcd944dce4f477962a82b7718795
hareshp <haresh.p@ezdi.us>
2018-07-06 19:48:14 +0530

    first commit

--- src/main/java/com/springbootadmin/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/com/springbootadmin/springbootadmin/SpringBootAdminApplication.java
@@ -1,9 +1,11 @@
 package com.springbootadmin.springbootadmin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
